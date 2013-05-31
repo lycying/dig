694236a724bf435d811f5b6a17f20d66dd64af18
liguo <lycying@gmail.com>
2013-05-31 10:46:25 +0800

    more good ui

--- digs/src/info/u250/digs/scenes/StartUpScene.java
+++ digs/src/info/u250/digs/scenes/StartUpScene.java
@@ -5,6 +5,8 @@ import info.u250.c2d.engine.SceneStage;
 import info.u250.c2d.graphic.background.SimpleMeshBackground;
 import info.u250.c2d.graphic.parallax.ParallaxGroup;
 import info.u250.c2d.graphic.parallax.ParallaxLayer;
+import info.u250.c2d.graphic.surfaces.SurfaceData;
+import info.u250.c2d.graphic.surfaces.TriangleSurfaces;
 import info.u250.digs.DigsEngineDrive;
 import info.u250.digs.scenes.game.Terrain;
 import info.u250.digs.scenes.game.TerrainConfig;
@@ -14,6 +16,7 @@ import com.badlogic.gdx.Application.ApplicationType;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.math.Vector2;
@@ -23,6 +26,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Array;
 
 public class StartUpScene extends SceneStage{
 	DigsEngineDrive drive;
@@ -30,28 +34,58 @@ public class StartUpScene extends SceneStage{
 	Terrain terrain;
 	float deltaAppend;
 	SimpleMeshBackground meshBackground ;
+	final TriangleSurfaces surface;
 	public StartUpScene(DigsEngineDrive drive){
 		this.drive = drive;
 		
 		atlas = Engine.resource("All");
 		
-		meshBackground = new SimpleMeshBackground(new Color(1, 1, 1, 1f),new Color(152f/255f, 181f/255f, 249f/255f, 1));
+		meshBackground = new SimpleMeshBackground(new Color(0, 0, 0, 1f),new Color(152f/255f, 181f/255f, 249f/255f, 1));
 
 		
 		final ParallaxGroup pbg = new ParallaxGroup(Engine.getWidth(), Engine.getHeight(), new Vector2(-50,0));
 		pbg.addActor(new ParallaxLayer(pbg, new Image(atlas.findRegion("cloud")), new Vector2(1,1), new Vector2(30,1000), new Vector2(0,350)));
-		pbg.addActor(new ParallaxLayer(pbg, new Image(atlas.findRegion("ground")), new Vector2(0.3f,1), new Vector2(0,1000), new Vector2(0,0)));
+//		pbg.addActor(new ParallaxLayer(pbg, new Image(atlas.findRegion("ground")), new Vector2(0.3f,1), new Vector2(0,1000), new Vector2(0,0)));
 		this.addActor(pbg);
+		
+		
+		final SurfaceData data = new SurfaceData();
+		data.primitiveType = GL10.GL_TRIANGLE_STRIP;
+		data.texture="Texture";
+		data.points = new Array<Vector2>(){{
+			add(new Vector2(-27.005554f,87f));
+			add(new Vector2(-20,-4));
+			add(new Vector2(119,250));
+			add(new Vector2(200.99362f,-14f));
+			add(new Vector2(293.00104f,300));
+			add(new Vector2(356f,-9f));
+			add(new Vector2(360f,310));
+			add(new Vector2(380,-9f));
+			add(new Vector2(400,300));
+			add(new Vector2(458f,-9f));
+			add(new Vector2(510f,280));
+			add(new Vector2(556.0f,-7f));
+			add(new Vector2(593f,270));
+			add(new Vector2(650f,-53f));
+			add(new Vector2(700f,290));
+			add(new Vector2(735f,-53f));
+			add(new Vector2(800f,290));
+			add(new Vector2(850f,0));
+			add(new Vector2(900f,290));
+			add(new Vector2(960f,0));
+			add(new Vector2(1024,350));
+		}};
+		surface  = new TriangleSurfaces(data);
 		
 		GreenHat greenHat = new GreenHat();
 		greenHat.setScale(13);
 		greenHat.setShowHp(false);
 		greenHat.setAnimation(greenHat.getGoldAnimationLeft());
-		greenHat.setPosition(780, 195);
+		greenHat.setPosition(720, 290);
 		this.addActor(greenHat);
 		
 		TerrainConfig config = new TerrainConfig();
-		config.surfaceFile = "texs/DSRT.png";
+		config.surfaceFile = "texs/LoamWalls.jpg";
 //		config.surfaceFile = "data/DSRT.png";
 		config.runnerNumber = 20;
 		config.segment = 8;
@@ -59,16 +93,16 @@ public class StartUpScene extends SceneStage{
 		terrain = new Terrain(config);
 		this.addActor(terrain);
 		Image logo = new Image(atlas.findRegion("logo"));
-		logo.setPosition(490, 400);
-//		logo.addAction(Actions.forever(Actions.sequence(
-//				Actions.rotateBy(5, 0.1f),
-//				Actions.rotateBy(-5, 0.1f,Interpolation.swingOut)
-//		)));
+		logo.setPosition(570, 120);
+		logo.addAction(Actions.forever(Actions.sequence(
+				Actions.moveBy(0,-50,0.5f,Interpolation.swingIn),
+				Actions.moveBy(0,50,0.5f,Interpolation.swingOut)
+		)));
 		this.addActor(logo);
 		
 		
 		Image grass = new Image(atlas.findRegion("grass"));
-		grass.setPosition(Engine.getWidth()/2-grass.getWidth()/2, -150);
+		grass.setPosition(Engine.getWidth()/2-grass.getWidth()/2, -100);
 		this.addActor(grass);
 		
 		Image water = new Image(atlas.findRegion("water"));
@@ -120,6 +154,8 @@ public class StartUpScene extends SceneStage{
 	@Override
 	public void draw() {
 		meshBackground.render(Engine.getDeltaTime());
+		surface.render(Engine.getDeltaTime());
+
 		super.draw();
 	}
 	@Override
